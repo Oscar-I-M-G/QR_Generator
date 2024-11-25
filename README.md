2552871f1a8f4cc63fd90419ab2f4176292cc225
Oscar-I-M-G <oscar.moreno4275@alumnos.udg.mx>
2024-11-25 12:40:45 -0600

    - Added GUI to generate QR

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ I wanted a simple QR code generator for me .
 Important to 
 pip install qrcode
 pip install pillow
+pip install pysimplegui
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import qrcode 
+import qrcode as qrc
+import PySimpleGUI as ui
 
-img = qrcode.make("https://oscar-i-m-g.github.io/portafolio/")
-type(img)
-img.save("portafolio.png")
+
+layout = [ 
+            [ui.Text("Insert link to generate QR code")],
+            [ui.InputText(key="link")],
+            [ui.Button("Generar código QR")]    ]
+
+window = ui.Window("QR Code Generator", layout)
+
+def generate_qr(link_to_generate):
+    result = qrc.make(link_to_generate)
+    type(result)
+    result.save("qr.png")
+    ui.popup("QR code generated successfully")
+
+
+
+while True:
+    event, values = window.read()
+    if event == ui.WIN_CLOSED:
+        break
+    if event == "Generar código QR":
+        link = values["link"]
+        generate_qr(link)
+        
+
+window.close()
