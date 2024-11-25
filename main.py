1e534db66cf80701183f68e757f0a41f44bc8831
Oscar-I-M-G <oscar.moreno4275@alumnos.udg.mx>
2024-11-25 15:20:03 -0600

    - paused the ui with tkinter

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import qrcode as qrc
+#from tkinter import *
+#from tkinter import ttk
 import PySimpleGUI as ui
 
+def generate_qr(link_to_generate):
+    result = qrc.make(link_to_generate)
+    type(result)
+    result.save("qr.png")
 
+## Tkinter
+#root =Tk()
+
+""""
+## Personalization
+root.title("QR Code Generator")
+root.geometry("400x400")
+
+mainframe = ttk.Frame(root, padding="12 12 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+
+
+"""
 layout = [ 
             [ui.Text("Insert link to generate QR code")],
             [ui.InputText(key="link")],
@@ -9,24 +31,21 @@ layout = [
             [ui.Image(key="qr_image")],
             [ui.Button("Close")]]
 
-window = ui.Window("QR Code Generator", layout)
 
-def generate_qr(link_to_generate):
-    result = qrc.make(link_to_generate)
-    type(result)
-    result.save("qr.png")
+window = ui.Window("QR Code Generator", layout)
 
 def update_window():
     window["qr_image"].update("qr.png")
 
 while True:
     event, values = window.read()
-    if event == ui.WIN_CLOSED or event == "Close":
+    if event == "Close" or event == ui.WIN_CLOSED:
         break
     if event == "Generar código QR":
-        link = values["link"]
-        generate_qr(link)
+        generate_qr(values["link"])
         update_window()
+
+#root.mainloop()
         
 
 window.close()
