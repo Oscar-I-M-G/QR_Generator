a84465a769d06e4e6be06abac748ed9fcb5eb9b4
Oscar-I-M-G <oscar.moreno4275@alumnos.udg.mx>
2024-11-25 12:52:47 -0600

    - Updated GUI

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import PySimpleGUI as ui
 layout = [ 
             [ui.Text("Insert link to generate QR code")],
             [ui.InputText(key="link")],
-            [ui.Button("Generar código QR")]    ]
+            [ui.Button("Generar código QR")],
+            [ui.Image(key="qr_image")],
+            [ui.Button("Close")]]
 
 window = ui.Window("QR Code Generator", layout)
 
@@ -13,17 +15,18 @@ def generate_qr(link_to_generate):
     result = qrc.make(link_to_generate)
     type(result)
     result.save("qr.png")
-    ui.popup("QR code generated successfully")
 
-
+def update_window():
+    window["qr_image"].update("qr.png")
 
 while True:
     event, values = window.read()
-    if event == ui.WIN_CLOSED:
+    if event == ui.WIN_CLOSED or event == "Close":
         break
     if event == "Generar código QR":
         link = values["link"]
         generate_qr(link)
+        update_window()
         
 
 window.close()
